3a975b29b8eb4ec3fd1e38f7711069368cebbfff
xtoazt <183557618+xtoazt@users.noreply.github.com>
2025-04-07 23:01:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { createServer } from "node:http";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { server as wisp } from "@mercuryworkshop/wisp-js";
+import "ws";
+
+console.log("Starting Terbium...");
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static("static"));
+const server = createServer();
+
+server.on("request", (req, res) => {
+  app(req, res);
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (req.url.endsWith("/wisp/")) {
+    wisp.routeRequest(req, socket, head);
+  }
+});
+
+const port = parseInt("6969");
+const manifest = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8');
+const { version } = JSON.parse(manifest);
+server.listen(port, () => {
+  console.log(`
+\x1b[38;2;50;174;98m@@@@@@@@@@@@@@~ B@@@@@@@@#G?.       
+\x1b[38;2;50;174;98mB###&@@@@&####^ #@@@&PPPB@@@G.      
+\x1b[38;2;50;174;98m .. ~@@@@J ..  .#@@@P   ~&@@@^      \x1b[38;2;60;195;240mWelcome to Legacy Terbium v${version}
+    \x1b[38;2;50;174;98m^@@@@?     .#@@@@###&@@&7       
+    \x1b[38;2;50;174;98m^@@@@?     .#@@@#555P&@@B7      \x1b[38;2;182;182;182mLegacy Terbium is running on ${port}
+    \x1b[38;2;50;174;98m^@@@@?     .#@@@P    G@@@@      \x1b[38;2;182;182;182mAny problems you encounter let us know!
+    \x1b[38;2;50;174;98m^@@@@?     .#@@@&GGG#@@@@Y      
+    \x1b[38;2;50;174;98m^&@@@?      B@@@@@@@@&B5~       
+  `);
+});
+
+process.on("SIGINT", () => {
+  console.log("\x1b[0m");
+  process.exit();
+});
